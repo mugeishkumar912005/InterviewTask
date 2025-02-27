c117743a76be959aae74237da56f3a2532045243
mugeishkumar2005 <152947329+mugeishkumar912005@users.noreply.github.com>
2025-02-27 19:06:15 +0530

    Update vite.config.js

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", 
-  build: {
-    outDir: "dist", 
-  },
+  base: "https://github.com/mugeishkumar912005/InterviewTask.git"
 });
